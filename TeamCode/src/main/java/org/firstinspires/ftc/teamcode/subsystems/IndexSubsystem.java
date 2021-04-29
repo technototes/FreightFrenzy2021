8798553238df0800b4bc831a30db24de11e78df7
sgeigerk <55821008+sgeigerk@users.noreply.github.com>
2021-04-29 16:26:54 -0700

    RemovePivot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IndexSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IndexSubsystem.java
@@ -18,38 +18,18 @@ public class IndexSubsystem extends ServoSubsystem implements Stated<String>{
             return pos;
         }
     }
-    public enum PivotPositon{
-        RAISED(0.65), LOWERED(0.1);
-        double pos;
-        PivotPositon(double v){
-            pos = v;
-        }
-        public double getPositon(){
-            return pos;
-        }
-    }
+ 
 
 
-
-    public Servo pivot, arm;
-    public PivotPositon pivotPositon = PivotPositon.LOWERED;
+    public Servo  arm;
     public ArmPositon armPositon = ArmPositon.RETRACTED;
 
-    public IndexSubsystem(Servo p, Servo a){
-        super(p, a);
-        pivot = p;
+    public IndexSubsystem(Servo a){
+        super(a);
         arm = a;
     }
     //subsystem functions
-    public void raiseToShooter(){
-        pivot.setPosition(PivotPositon.RAISED.getPositon());
-        pivotPositon = PivotPositon.RAISED;
-    }
-    public void lowerToIntake(){
-        pivot.setPosition(PivotPositon.LOWERED.getPositon());
-        pivotPositon = PivotPositon.LOWERED;
-    }
-    public void extendArm(){
+        public void extendArm(){
         arm.setPosition(ArmPositon.EXTENDED.getPositon());
         armPositon = ArmPositon.EXTENDED;
     }
@@ -61,6 +41,6 @@ public class IndexSubsystem extends ServoSubsystem implements Stated<String>{
 
     @Override
     public String getState() {
-        return "PIVOT: "+pivotPositon+". ARM: "+armPositon;
+        return "ARM: "+armPositon;
     }
 }
