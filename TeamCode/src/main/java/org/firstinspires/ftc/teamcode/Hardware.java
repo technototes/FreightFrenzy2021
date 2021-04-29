eb495504feac00a88801b0911f5861ea22860894
sgeigerk <55821008+sgeigerk@users.noreply.github.com>
2021-04-29 16:28:30 -0700

    RemovePivot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -36,7 +36,6 @@ public class Hardware implements Loggable {
 
     //index
     public Servo indexArmServo;
-    public Servo indexPivotServo;
 
     //intake
     public Motor intakeMotor1;
@@ -70,7 +69,6 @@ public class Hardware implements Loggable {
         imu = new IMU("imu");
 
         indexArmServo = new Servo("indexarm");
-        indexPivotServo = new Servo("indexpivot");
 
         intakeMotor1 = new Motor<>("intake1").invert();
         intakeMotor2 = new Motor<>("intake2").invert();
