e3193ea7e356225b4ee7b41029c745cecf36fd6f
1198159 <jeffastedman@gmail.com>
2021-10-08 12:34:17 -0700

    thingy

---
+++ desktop/src/com/technototes/simulator/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.technototes.simulator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.technototes.simulator.Simulator;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Simulator(), config);
+	}
+}
