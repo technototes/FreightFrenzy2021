b359d404bb4777987d37790df450c2a1eeaa4f9d
1toldyou <1173115791@qq.com>
2021-10-19 17:09:26 -0700

    lift again

--- TechnoOneCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TechnoOneCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.teamcode.subsystems.DepositSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.LiftSubsystem;
 
 public class Robot {
     public LiftSubsystem liftSubsystem;
+    public DepositSubsystem depositSubsystem;
     public Robot(){
-
+        liftSubsystem = new LiftSubsystem(Hardware.liftMotor);
+        depositSubsystem = new DepositSubsystem(Hardware.leftDepositServo, Hardware.rightDepositServo);
     }
 }
