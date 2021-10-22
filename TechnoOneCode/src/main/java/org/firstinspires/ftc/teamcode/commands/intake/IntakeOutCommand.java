5b6f921d787d0355d9828b81f896fdcd11a7b75a
tiotuico2 <ryetyo@yahoo.com>
2021-10-22 16:25:28 -0700

    made commands for intake subsystem

--- TechnoOneCode/src/main/java/org/firstinspires/ftc/teamcode/commands/intake/IntakeOutCommand.java
+++ TechnoOneCode/src/main/java/org/firstinspires/ftc/teamcode/commands/intake/IntakeOutCommand.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode.commands.intake;
 
-public class IntakeOutCommand {
+import com.technototes.library.command.Command;
+
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+
+public class IntakeOutCommand implements Command{
+    IntakeSubsystem subsystem;
+    public IntakeOutCommand(IntakeSubsystem s){
+        subsystem = s;
+        addRequirements(s);
+    }
+
+    @Override
+    public void execute() {
+        subsystem.out();
+    }
+
 }
--- TechnoOneCode/src/main/java/org/firstinspires/ftc/teamcode/commands/intake/IntakeStopCommand.java
+++ TechnoOneCode/src/main/java/org/firstinspires/ftc/teamcode/commands/intake/IntakeStopCommand.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.commands.intake;
 
-public class IntakeStopCommand {
+import com.technototes.library.command.Command;
+
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+
+public class IntakeStopCommand implements Command {
+    IntakeSubsystem subsystem;
+    public IntakeStopCommand(IntakeSubsystem s){
+        subsystem = s;
+        addRequirements(s);
+    }
+
+    @Override
+    public void execute() {
+        subsystem.stop();
+    }
 }
