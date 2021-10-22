f9d3796912f69d42ca0c1ec3cb08e5da802f9e0e
migaorca <miga-e@live.com>
2021-10-22 16:42:58 -0700

    Finished Code for Carousel Subsystem

--- TechnoOneCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CarouselSubsystem.java
+++ TechnoOneCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CarouselSubsystem.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.technototes.library.hardware.motor.Motor;
 import com.technototes.library.subsystem.Subsystem;
@@ -9,11 +10,23 @@ import java.util.function.Supplier;
 @SuppressWarnings("unused")
 
 public class CarouselSubsystem implements Subsystem, Supplier<Double> {
-    public Motor<DcMotorEx> motor;
-    public CarouselSubsystem(Motor<DcMotorEx> m){
-        motor = m;
+
+    @Config
+    public static class CarouselConstants{
+        public static double CAROUSEL_RIGHT_SPEED = 1;
+        public static double CAROUSEL_LEFT_SPEED = -1;
+        public static double CAROUSEL_STOP_SPEED = 0;
     }
 
+    public Motor<DcMotorEx> motor;
+
+    public CarouselSubsystem(Motor<DcMotorEx> m){motor = m;}
+
+    public void right(){motor.setSpeed(CAROUSEL_RIGHT_SPEED);}
+
+    public void left(){motor.setSpeed(CAROUSEL_LEFT_SPEED);}
+
+    public void stop(){motor.setSpeed(CAROUSEL_STOP_SPEED);}
     @Override
     public Double get() {
         return motor.getSpeed();
