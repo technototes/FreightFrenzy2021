083cb95740884256915814ad461f0b92bd9e5e48
tiotuico2 <ryetyo@yahoo.com>
2021-11-05 17:41:24 -0700

    got the carousel subsystem and commands done - done by andrew hardware also pasted in - but wonky

--- TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/carousel/CarouselStopCommand.java
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/carousel/CarouselStopCommand.java
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.teamcode.commands.carousel;
 
-public class CarouselStopCommand {
+import com.technototes.library.command.Command;
+
+import org.firstinspires.ftc.teamcode.subsystems.CarouselSubsystem;
+
+public class CarouselStopCommand implements Command{
+
+    private CarouselSubsystem subsystem;
+
+    public CarouselStopCommand (CarouselSubsystem s) {
+        subsystem = s;
+        addRequirements(s);
+    }
+
+    @Override
+    public void execute() {
+        subsystem.stop();
+    }
 }
--- TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CarouselSubsystem.java
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CarouselSubsystem.java
@@ -1,9 +1,41 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.teamcode.subsystems.CarouselSubsystem.CarouselConstants.*;
+
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.technototes.library.hardware.motor.Motor;
+import com.technototes.library.subsystem.Subsystem;
 
-public class CarouselSubsystem {
-  public CarouselSubsystem(Motor<DcMotorEx> m){}
+import java.util.function.Supplier;
 
+
+public class CarouselSubsystem implements Subsystem, Supplier<Double> {
+
+  @Config
+  public static class CarouselConstants{
+    public static double CAROUSEL_RIGHT_SPEED = 1;
+    public static double CAROUSEL_LEFT_SPEED = -1;
+    public static double CAROUSEL_STOP_SPEED = 0;
+  }
+
+  public Motor<DcMotorEx> motor;
+  public CarouselSubsystem(Motor<DcMotorEx> m){
+    motor = m;
+  }
+  public void right() {
+    motor.setSpeed(CAROUSEL_RIGHT_SPEED);
+  }
+
+
+  public void left() {
+    motor.setSpeed(CAROUSEL_LEFT_SPEED);
+  }
+  public void stop() {
+    motor.setSpeed(CAROUSEL_STOP_SPEED);
+  }
+  @Override
+  public Double get() {
+    return motor.getSpeed();
+  }
 }
