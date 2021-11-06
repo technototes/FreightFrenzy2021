4106049b5b7d07610174f54b94156d9f4418509b
migaorca <miga-e@live.com>
2021-11-05 17:15:58 -0700

    IntakeSubsystem finished + Commands: Ryan Tang

--- TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/intake/IntakeOutCommand.java
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/intake/IntakeOutCommand.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.commands.intake;
 
-public class IntakeOutCommand {
+import com.technototes.library.command.Command;
+
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+
+public class IntakeOutCommand implements Command {
+    IntakeSubsystem subsystem;
+
+    public IntakeOutCommand(IntakeSubsystem s) {
+        subsystem = s;
+        addRequirements(s);
+    }
+    @Override
+    public void execute() {
+        subsystem.out();
+    }
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    @Override
+    public void end(boolean cancel) {
+        subsystem.stop();
+    }
 }
--- TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VisionSubsystem.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.technototes.vision.hardware.Camera;
 import com.technototes.vision.subsystem.PipelineSubsystem;
 
@@ -8,9 +9,18 @@ import org.opencv.core.Mat;
 import java.util.function.Supplier;
 
 public class VisionSubsystem extends PipelineSubsystem implements Supplier<Integer> {
+  @Config
+  public static class VisionSubsystemConstants{
+    public static double RANGE_CORRECTION;
+    public static double LEFT_RANGE, MIDDLE_RANGE, RIGHT_RANGE;
+  }
 
   public VisionSubsystem(Camera c) {
     super(c);
+  }
+
+  public double get_green_position() {
+    return 0.0;
   }
 
   @Override
