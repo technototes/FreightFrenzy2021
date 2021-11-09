37b6d17db05ce273d24d8baa707b339b8e8c7eff
tiotuico2 <ryetyo@yahoo.com>
2021-11-08 18:10:52 -0800

    vision code has made progress, not done

--- TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMode.java
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMode.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.technototes.library.logger.Loggable;
 import com.technototes.library.structure.CommandOpMode;
