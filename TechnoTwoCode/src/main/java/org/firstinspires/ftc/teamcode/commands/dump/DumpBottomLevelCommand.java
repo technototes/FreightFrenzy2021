ef854bdee82821f77b8b7a7389caba801d4472f5
tiotuico2 <ryetyo@yahoo.com>
2021-11-13 13:24:00 -0800

    stuff

--- TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/dump/DumpBottomLevelCommand.java
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/dump/DumpBottomLevelCommand.java
@@ -2,12 +2,10 @@ package org.firstinspires.ftc.teamcode.commands.dump;
 
 import com.technototes.library.command.SequentialCommandGroup;
 
-import org.firstinspires.ftc.teamcode.commands.arm.ArmCollectedCommand;
-import org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem;
+
 
 public class DumpBottomLevelCommand extends SequentialCommandGroup {
-    public DumpBottomLevelCommand(ArmSubsystem arm, DumpSubsystem dump){
-        super (new ArmCollectedCommand(arm), new q)
+
     }
 
 }
