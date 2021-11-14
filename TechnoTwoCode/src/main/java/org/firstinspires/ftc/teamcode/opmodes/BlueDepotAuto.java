fcf605b7ddc53c1a8de97c43b4aac582c3cbfb8c
Kevin Frei <kevinfrei@hotmail.com>
2021-11-13 22:29:33 -0800

    Added the Auto tele-ops to expose Ryan's initial (and fully untested) auto code

---
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BlueDepotAuto.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.logger.Loggable;
+import com.technototes.library.structure.CommandOpMode;
+import com.technototes.library.util.Alliance;
+
+import org.firstinspires.ftc.teamcode.Hardware;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.commands.autonomous.AutonomousConstants;
+import org.firstinspires.ftc.teamcode.commands.autonomous.AutonomousShippingHubCommandGroup;
+
+@Autonomous(name = "Blue Depot")
+public class BlueDepotAuto extends CommandOpMode implements Loggable {
+    public Robot robot;
+    public Hardware hardware;
+
+    @Override
+    public void uponInit() {
+        AutonomousConstants.ALLIANCE = Alliance.BLUE;
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        hardware = new Hardware();
+        robot = new Robot(hardware);
+        robot.drivebaseSubsystem.setPoseEstimate(AutonomousConstants.SHIPPING_HUB_START_SELECT.get());
+
+        CommandScheduler.getInstance().scheduleForState(
+                new AutonomousShippingHubCommandGroup(robot.drivebaseSubsystem, robot.dumpSubsystem),
+                CommandOpMode.OpModeState.RUN);
+    }
+}
