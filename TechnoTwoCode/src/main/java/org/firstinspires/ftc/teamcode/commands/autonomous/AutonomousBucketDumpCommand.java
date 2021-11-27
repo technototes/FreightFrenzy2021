b5edb4f8b54bb5a0ad6b1dc0273d7743551534fa
James Dooley <jdooley4@hotmail.com>
2021-11-27 14:00:34 -0800

    Update auto commands to use v2 of Commands & rotate at end to match expected driver position Delete v1 commands so they aren't accidentally used.

--- TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousBucketDumpCommand.java
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousBucketDumpCommand.java
@@ -3,17 +3,14 @@ package org.firstinspires.ftc.teamcode.commands.autonomous;
 import com.technototes.library.command.SequentialCommandGroup;
 import com.technototes.library.command.WaitCommand;
 
-import org.firstinspires.ftc.teamcode.commands.bucket.BucketCarryCommand;
-import org.firstinspires.ftc.teamcode.commands.bucket.BucketUnloadTopLevelCommand;
-import org.firstinspires.ftc.teamcode.commands.dump.DumpCarryCommand;
 import org.firstinspires.ftc.teamcode.commands.dump.DumpCarryCommand2;
-import org.firstinspires.ftc.teamcode.commands.dump.DumpUnloadTopLevelCommand;
+import org.firstinspires.ftc.teamcode.commands.dump.DumpUnloadTopLevelCommand2;
 import org.firstinspires.ftc.teamcode.subsystems.DumpSubsystem;
 
 public class AutonomousBucketDumpCommand extends SequentialCommandGroup {
     public AutonomousBucketDumpCommand(DumpSubsystem bucket) {
         super(new DumpCarryCommand2(bucket),
-                  new DumpUnloadTopLevelCommand(bucket),
+                  new DumpUnloadTopLevelCommand2(bucket),
                   new WaitCommand(1));
     }
 }
