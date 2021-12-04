3537a04773a2f54484ff0010d105e48f15a1c7ec
tiotuico2 <ryetyo@yahoo.com>
2021-12-04 09:36:33 -0800

    Merge in the 2 removal

--- SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/HeightSelectCommand.java
+++ SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/HeightSelectCommand.java
@@ -10,8 +10,8 @@ import org.firstinspires.ftc.teamcode.subsystems.VisionSubsystem;
 
 public class HeightSelectCommand extends ChoiceCommand {
     public HeightSelectCommand(VisionSubsystem vision, DumpSubsystem dump){
-        super(new Pair<>(vision.barcodePipeline::zero, new DumpUnloadBottomLevelCommand2(dump)),
-                new Pair<>(vision.barcodePipeline::one, new DumpUnloadMiddleLevelCommand2(dump)),
-                new Pair<>(vision.barcodePipeline::two, new DumpUnloadTopLevelCommand2(dump)));
+        super(new Pair<>(vision.barcodePipeline::zero, new DumpUnloadBottomLevelCommand(dump)),
+                new Pair<>(vision.barcodePipeline::one, new DumpUnloadMiddleLevelCommand(dump)),
+                new Pair<>(vision.barcodePipeline::two, new DumpUnloadTopLevelCommand(dump)));
     }
 }
