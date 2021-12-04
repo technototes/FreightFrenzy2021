ca72042765b875790c7000eb5f2fce0a54dd5c9c
1toldyou <86680163+1toldyou@users.noreply.github.com>
2021-12-04 11:00:35 -0800

    Fixed a constant

--- TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutoBlueShippingHubCommandGroup.java
+++ TechnoTwoCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutoBlueShippingHubCommandGroup.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.teamcode.subsystems.VisionSubsystem;
 public class AutoBlueShippingHubCommandGroup extends SequentialCommandGroup {
     public AutoBlueShippingHubCommandGroup(DrivebaseSubsystem drive, DumpSubsystem bucket, VisionSubsystem vision) {
         super(new DumpCarryCommand(bucket),
-                new TrajectorySequenceCommand(drive, AutonomousConstants.BLUE_START_TO_SHIPPING_HUB), // Different duck constant
+                new TrajectorySequenceCommand(drive, AutonomousConstants.BLUE_DEPOT_START_TO_ALLIANCE_HUB_LEVEL3), // Different duck constant
                 new HeightSelectCommand(vision, bucket).withTimeout(3), // Bucket command
                 new WaitCommand(1),
                 new DumpCollectCommand(bucket),
