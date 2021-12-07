59e683d6769780a0286644d89336521bc0a6c938
1198159 <jeffastedman@gmail.com>
2021-12-07 10:28:06 -0800

    updated lib to dev version and deleted not done code for penguin yet

--- PenguinCode/src/main/java/org/firstinspires/ftc/teamcode/commands/lift/LiftLevel3Command.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.commands.lift;
-
-import org.firstinspires.ftc.teamcode.subsystems.LiftSubsystem;
-
-public class LiftLevel3Command extends LiftCommand {
-    public LiftLevel3Command(LiftSubsystem l){
-        super(l, LiftSubsystem.LiftConstants.LEVEL_3);
-    }
-}
