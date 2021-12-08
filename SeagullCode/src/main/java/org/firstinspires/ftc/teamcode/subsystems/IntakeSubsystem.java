226e1a4af2dcdd17c28942ae43be7d99c3fdc3cd
1toldyou <86680163+1toldyou@users.noreply.github.com>
2021-12-07 17:35:16 -0800

    made Red Depot more reliable and faster

--- SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
+++ SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -9,8 +9,8 @@ import java.util.function.Supplier;
 
 public class IntakeSubsystem implements Subsystem, Supplier<Double> {
   public static class IntakeConstant {
-    public static double INTAKE_IN_SPEED = -0.8;
-    public static double INTAKE_OUT_SPEED = 0.8;
+    public static double INTAKE_IN_SPEED = -0.825;
+    public static double INTAKE_OUT_SPEED = 0.825;
     public static double INTAKE_STOP_SPEED = 0;
   }
   public EncodedMotor<DcMotorEx> motor;
