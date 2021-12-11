52eb058e32f9f8d6cef51566cef65c2230b1e090
James Dooley <jdooley4@hotmail.com>
2021-12-11 10:24:19 -0800

    Change bucket dump to dump more

--- SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DumpSubsystem.java
+++ SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DumpSubsystem.java
@@ -45,7 +45,7 @@ public class DumpSubsystem implements Subsystem, Supplier<Double>, Loggable {
          */
         static final double BUCKET_COLLECT = 0.05;
         static final double BUCKET_CARRY = 0.3;
-        static final double BUCKET_DUMP = 0.7;
+        static final double BUCKET_DUMP = 0.75;
 
         // Range of values where the bucket should be in the carry position
         static final double ARM_CARRY_LIMIT_MIN = (ARM_COLLECT + ARM_CARRY) / 3;
