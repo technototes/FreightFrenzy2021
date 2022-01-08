de16f4ec7ebf59e2c45f3b6cf93f11d4e3ef3453
tiotuico2 <ryetyo@yahoo.com>
2022-01-07 18:19:52 -0800

    have some command groups for vision code, untested

--- SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutoRedShippingHubBackAndForthCommandGroup.java
+++ SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutoRedShippingHubBackAndForthCommandGroup.java
@@ -7,6 +7,7 @@ import com.technototes.path.command.TrajectorySequenceCommand;
 
 import org.firstinspires.ftc.teamcode.commands.dump.DumpCarryCommand;
 import org.firstinspires.ftc.teamcode.commands.dump.DumpCollectCommand;
+import org.firstinspires.ftc.teamcode.commands.dump.DumpUnloadTopLevelCommand;
 import org.firstinspires.ftc.teamcode.subsystems.DumpSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.DrivebaseSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
@@ -15,7 +16,7 @@ public class AutoRedShippingHubBackAndForthCommandGroup extends SequentialComman
     public AutoRedShippingHubBackAndForthCommandGroup(DrivebaseSubsystem drive, DumpSubsystem bucket, IntakeSubsystem intake) {
         super(new DumpCarryCommand(bucket),
                 new TrajectorySequenceCommand(drive, AutonomousConstants.RED_DEPOT_START_TO_ALLIANCE_HUB_LEVEL3), // Different duck const
-                new AutonomousBucketDumpCommand(bucket).withTimeout(3), // Bucket command
+                new DumpUnloadTopLevelCommand(bucket).withTimeout(3), // Bucket command
                 new WaitCommand(0.5),
                 new DumpCollectCommand(bucket),
                 new AutonomousLoopCommandGroup(drive,bucket,intake),
--- SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousLoopCommandGroup.java
+++ SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousLoopCommandGroup.java
@@ -5,6 +5,7 @@ import com.technototes.library.command.WaitCommand;
 import com.technototes.path.command.TrajectorySequenceCommand;
 
 import org.firstinspires.ftc.teamcode.commands.dump.DumpCollectCommand;
+import org.firstinspires.ftc.teamcode.commands.dump.DumpUnloadTopLevelCommand;
 import org.firstinspires.ftc.teamcode.commands.intake.IntakeInCommand;
 import org.firstinspires.ftc.teamcode.commands.intake.IntakeStopCommand;
 import org.firstinspires.ftc.teamcode.subsystems.DumpSubsystem;
@@ -14,7 +15,7 @@ import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
 public class AutonomousLoopCommandGroup extends SequentialCommandGroup {
     public AutonomousLoopCommandGroup(DrivebaseSubsystem drive, DumpSubsystem bucket, IntakeSubsystem intake){
         super(new DumpCollectCommand(bucket),
-            new AutonomousBucketDumpCommand(bucket).withTimeout(3), // Bucket command
+            new DumpUnloadTopLevelCommand(bucket).withTimeout(3), // Bucket command
                 new WaitCommand(0.5),
                 new DumpCollectCommand(bucket),
                 new TrajectorySequenceCommand(drive, AutonomousConstants.RED_ALLIANCE_HUB_LEVEL3_TO_DEPOT_COLLECT1),
