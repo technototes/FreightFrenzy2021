dba47aa52ae1b29b617dadda13f1a4a8eee08677
migaorca <miga-e@live.com>
2022-01-08 10:53:23 -0800

    Work On Red Duck Auto

--- SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousConstants.java
+++ SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousConstants.java
@@ -17,7 +17,9 @@ public class AutonomousConstants {
         public static Pose2d DUCK_START = new Pose2d(-36, -63, toRadians(-90));
         public static Pose2d DUCK_ALLIANCE_HUB_LEVEL3 = new Pose2d(-28, -38, toRadians(-125));
         public static Pose2d DUCK_CAROUSEL = new Pose2d(-64, -60, toRadians(-90));
-        public static Pose2d DUCK_PARK = new Pose2d(-70, -33, toRadians(180));
+        public static Pose2d DUCK_COLLECT1 = new Pose2d(-55, -56, toRadians(-90));
+        public static Pose2d DUCK_COLLECT2 = new Pose2d(-55, -58, toRadians(-90));
+        public static Pose2d DUCK_PARK = new Pose2d(-69, -36, toRadians(180));
         public static Pose2d DEPOT_START = new Pose2d(0, -66, toRadians(-90));
         public static Pose2d DEPOT_ALLIANCE_HUB_LEVEL3 = new Pose2d(-10, -45, toRadians(-68));
         public static Pose2d DEPOT_GAP = new Pose2d(20, -67, toRadians(0));
@@ -39,7 +41,7 @@ public class AutonomousConstants {
     public static Alliance ALLIANCE = Alliance.BLUE;
 
     public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence>
-              // Lists of driving series for auto naviation
+              // Lists of driving series for auto navigation
               RED_DUCK_START_TO_ALLIANCE_HUB_LEVEL3 = b -> b.apply(RedConstants.DUCK_START)
                         .lineToLinearHeading(RedConstants.DUCK_ALLIANCE_HUB_LEVEL3)
                         .build(),
@@ -47,9 +49,22 @@ public class AutonomousConstants {
                         .lineToLinearHeading(RedConstants.DUCK_CAROUSEL)
                         .setAccelConstraint(getAccelerationConstraint(30))
                         .build(),
-              RED_DUCK_CAROUSEL_TO_PARK = b -> b.apply(RedConstants.DUCK_CAROUSEL)
-                        .lineToLinearHeading(RedConstants.DUCK_PARK)
+              RED_DUCK_CAROUSEL_TO_COLLECT1 = b -> b.apply(RedConstants.DUCK_CAROUSEL)
+                        .lineToLinearHeading(RedConstants.DUCK_COLLECT1)
                         .build(),
+              RED_DUCK_COLLECT1_TO_COLLECT2 = b -> b.apply(RedConstants.DUCK_COLLECT1)
+                      .lineToLinearHeading(RedConstants.DUCK_COLLECT2)
+                      .build(),
+              RED_DUCK_COLLECT2_TO_COLLECT1 = b -> b.apply(RedConstants.DUCK_COLLECT2)
+                    .lineToLinearHeading(RedConstants.DUCK_COLLECT1)
+                    .build(),
+              RED_DUCK_COLLECT2_TO_ALLIANCE_HUB_LEVEL3 = b -> b.apply(RedConstants.DUCK_COLLECT2)
+                      .lineToLinearHeading(RedConstants.DUCK_ALLIANCE_HUB_LEVEL3)
+                      .build(),
+              RED_DUCK_ALLIANCE_HUB_LEVEL3_TO_PARK = b -> b.apply(RedConstants.DUCK_ALLIANCE_HUB_LEVEL3)
+                      .lineToLinearHeading(RedConstants.DUCK_PARK)
+                      .build(),
+
               RED_DEPOT_START_TO_ALLIANCE_HUB_LEVEL3 = b -> b.apply(RedConstants.DEPOT_START)
                         .lineToLinearHeading(RedConstants.DEPOT_ALLIANCE_HUB_LEVEL3)
                         .build(),
