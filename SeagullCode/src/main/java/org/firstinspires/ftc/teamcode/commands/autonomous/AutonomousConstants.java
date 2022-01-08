1230c0327d70da6d5b6736be4663c0c4786eabb6
migaorca <miga-e@live.com>
2022-01-08 11:18:58 -0800

    Red duck auto can collect duck now!

--- SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousConstants.java
+++ SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousConstants.java
@@ -17,7 +17,7 @@ public class AutonomousConstants {
         public static Pose2d DUCK_START = new Pose2d(-36, -63, toRadians(-90));
         public static Pose2d DUCK_ALLIANCE_HUB_LEVEL3 = new Pose2d(-28, -38, toRadians(-125));
         public static Pose2d DUCK_CAROUSEL = new Pose2d(-64, -59, toRadians(-90));
-        public static Pose2d DUCK_COLLECT1 = new Pose2d(-55, -60, toRadians(-90));
+        public static Pose2d DUCK_COLLECT1 = new Pose2d(-60, -60, toRadians(-90));
         public static Pose2d DUCK_COLLECT2 = new Pose2d(-50, -60, toRadians(-90));
         public static Pose2d DUCK_PARK = new Pose2d(-69, -36, toRadians(180));
         public static Pose2d DEPOT_START = new Pose2d(0, -66, toRadians(-90));
