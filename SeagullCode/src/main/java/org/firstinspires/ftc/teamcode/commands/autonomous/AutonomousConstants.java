ff532fca1d64567739000a6769d5c4375a975ad6
big-beast-boss <malcolmbennitt@gmail.com>
2022-02-01 17:30:15 -0800

    blue depot auto works better

--- SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousConstants.java
+++ SeagullCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/AutonomousConstants.java
@@ -42,12 +42,12 @@ public class AutonomousConstants {
         public static Pose2d DUCK_COLLECT2 = new Pose2d(-43, 61, toRadians(90));
         public static Pose2d DUCK_PARK = new Pose2d(-69, 36, toRadians(180));
         public static Pose2d DEPOT_START = new Pose2d(0, 66, toRadians(90));
-        public static Pose2d DEPOT_ALLIANCE_HUB_LEVEL3 = new Pose2d(-10, 42, toRadians(55));
-        public static Pose2d DEPOT_ALLIANCE_HUB_LEVEL2 = new Pose2d(-10, 52, toRadians(60));
-        public static Pose2d DEPOT_ALLIANCE_HUB_LEVEL1 = new Pose2d(-8, 54, toRadians(60));
-        public static Pose2d DEPOT_GAP = new Pose2d(20, 67, toRadians(0));
-        public static Pose2d DEPOT_COLLECT1 = new Pose2d(46, 67, toRadians(0));
-        public static Pose2d DEPOT_COLLECT2 = new Pose2d(48, 67, toRadians(0));
+        public static Pose2d DEPOT_ALLIANCE_HUB_LEVEL3 = new Pose2d(-10, 42, toRadians(65));
+        public static Pose2d DEPOT_ALLIANCE_HUB_LEVEL2 = new Pose2d(-10, 44, toRadians(60));
+        public static Pose2d DEPOT_ALLIANCE_HUB_LEVEL1 = new Pose2d(-8, 44, toRadians(60));
+        public static Pose2d DEPOT_GAP = new Pose2d(20, 66.5, toRadians(0));
+        public static Pose2d DEPOT_COLLECT1 = new Pose2d(56, 67, toRadians(0));
+        public static Pose2d DEPOT_COLLECT2 = new Pose2d(58, 67, toRadians(0));
     }
 
     public static Alliance ALLIANCE = Alliance.BLUE;
